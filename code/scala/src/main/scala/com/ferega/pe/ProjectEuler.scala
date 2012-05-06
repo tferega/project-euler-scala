ee8fb02aaa768b308ae82db17761e8acacae43a2
Tomo Ferega <tomo@element.hr>
2012-05-06 20:01:29 +0200

    Problem 006 solution.

--- code/scala/src/main/scala/com/ferega/pe/ProjectEuler.scala
+++ code/scala/src/main/scala/com/ferega/pe/ProjectEuler.scala
@@ -5,6 +5,6 @@ import problems._
 object ProjectEuler {
   def main(args: Array[String]): Unit = {
     println("START")
-    Problem005.run
+    Problem006.run
   }
 }
---
+++ code/scala/src/main/scala/com/ferega/pe/problems/Problem006.scala
@@ -0,0 +1,29 @@
+package com.ferega.pe
+package problems
+
+/** The sum of the squares of the first ten natural numbers is,
+  * 12 + 22 + ... + 102 = 385
+  *
+  * The square of the sum of the first ten natural numbers is,
+  * (1 + 2 + ... + 10)2 = 552 = 3025
+  *
+  * Hence the difference between the sum of the squares of the first ten natural
+  * numbers and the square of the sum is 3025 − 385 = 2640.
+  *
+  * Find the difference between the sum of the squares of the first one hundred
+  * natural numbers and the square of the sum.
+  */
+object Problem006 extends Problem(6) {
+  def sq(i: Int) = i*i
+
+  def sumSq(l: Seq[Int]) =
+    l map sq sum
+  def sqSum(l: Seq[Int]) =
+    sq(l sum)
+
+
+  protected def solution = {
+    val l = 1 to 100
+    sqSum(l) - sumSq(l)
+  }
+}
